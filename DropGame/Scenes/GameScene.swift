a0c401a4505d6f34f4b3181da4132443634d900c
Max Aird <blackberry3224@gmail.com>
2022-11-10 12:44:02 -0700

    made blocks different colors and fall

--- DropGame/Scenes/GameScene.swift
+++ DropGame/Scenes/GameScene.swift
@@ -7,11 +7,14 @@
 
 import SpriteKit
 
-class GameScene : SKScene
+class GameScene : SKScene, SKPhysicsContactDelegate
 {
+    private var colorMask : Int = 0b0000
+    
     override func didMove(to view : SKView) -> Void
     {
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        physicsWorld.contactDelegate = self
     }
     
     override func touchesBegan(_ touches : Set<UITouch>, with event : UIEvent?) -> Void
@@ -19,7 +22,7 @@ class GameScene : SKScene
         guard let touch = touches.first
         else { return }
         
-        let currentColor = UIColor.purple
+        let currentColor = assignColorAndBitmask()
         let width = Int (arc4random() % 50)
         let height = Int (arc4random() % 50)
         let location = touch.location(in: self)
@@ -28,6 +31,15 @@ class GameScene : SKScene
         node = SKSpriteNode(color: currentColor, size: CGSize(width: width, height: height))
         node.position = location
         
+        node.physicsBody = SKPhysicsBody(rectangleOf:CGSize(width: width, height: height))
+        
         addChild(node)
+    }
+    
+    private func assignColorAndBitmask() -> UIColor{
+        let colors: [UIColor] = [.green, .black, .red, .orange, .systemPink, .darkGray, .blue, .cyan, .purple]
+        let randomIndex = Int(arc4random()) % colors.count
+        
+        return colors[randomIndex]
     }
 }
