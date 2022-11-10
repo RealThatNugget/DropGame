36a3ba7e05fbc6cd819d620bcf598fe56c1d9e00
Max Aird <blackberry3224@gmail.com>
2022-11-10 11:37:36 -0700

    first commit

---
+++ DropGame/DropGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropGameApp.swift
+//  DropGame
+//
+//  Created by Aird, Margaret on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct DropGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
