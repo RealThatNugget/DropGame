213449def1123fbabefa8b30c14e041d894e4dec
Max Aird <blackberry3224@gmail.com>
2022-11-10 12:05:10 -0700

    added override funcs to game scene

--- DropGame/Scenes/GameScene.swift
+++ DropGame/Scenes/GameScene.swift
@@ -9,5 +9,13 @@ import SpriteKit
 
 class GameScene : SKScene
 {
+    override func didMove(to view : SKView) -> Void
+    {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
     
+    override func touchesBegan(_ touches : Set<UITouch>, with event : UIEvent?) -> Void
+    {
+        
+    }
 }
